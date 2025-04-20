da86b7b09c66c0baf3cb2d918cd10cce6feec4f5
Sam Pham <52004834+samhpham@users.noreply.github.com>
2025-04-20 11:33:05 +0100

    Refactor IVs to interface (#22)

--- src/infrastructure/KiriphoritoPlayerPokemon.ts
+++ src/infrastructure/KiriphoritoPlayerPokemon.ts
@@ -1,9 +1,9 @@
 import { PlayerPokemon } from "../domain/model/PlayerPokemon";
 import { Gender } from "../domain/model/Pokemon";
-import { IndividualValues } from "../domain/model/IndividualValues";
+import * as IndividualValues from "../domain/model/IndividualValues";
 
 export const KiriphoritoPlayerPokemon: PlayerPokemon[] = [
-    new PlayerPokemon(46, Gender.Male, new IndividualValues(93)),
-    new PlayerPokemon(46, Gender.Female, new IndividualValues(80)),
-    new PlayerPokemon(46, Gender.Male,  new IndividualValues(80))
+    new PlayerPokemon(46, Gender.Male, IndividualValues.fromAverageIV(93)),
+    new PlayerPokemon(46, Gender.Female, IndividualValues.fromAverageIV(80)),
+    new PlayerPokemon(46, Gender.Male,  IndividualValues.fromAverageIV(80))
 ]
