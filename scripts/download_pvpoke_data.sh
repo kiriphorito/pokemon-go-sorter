a1d18c54182d936d32fcdbe1786544df082bb2f9
Sam Pham <kiriphorito@users.noreply.github.com>
2025-05-17 23:10:39 +0100

    Added retries and random wait to data script (#24)
    Co-authored-by: Sam Pham <sam.pham@transferwise.com>

--- scripts/download_pvpoke_data.sh
+++ scripts/download_pvpoke_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O ./data/pokemon.json 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/gamemaster/pokemon.json'
-wget -O ./data/great-overall-league.json 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-1500.json'
-wget -O ./data/ultra-overall-league.json 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-2500.json'
+wget -O ./data/pokemon.json --wait 10 --random-wait --continue 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/gamemaster/pokemon.json'
+wget -O ./data/great-overall-league.json --wait 10 --random-wait --continue 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-1500.json'
+wget -O ./data/ultra-overall-league.json --wait 10 --random-wait --continue 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-2500.json'
