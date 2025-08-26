df3c4164bbf3d4025f2dfad71b5e574083a970e1
Sam Pham <sam.pham@transferwise.com>
2025-08-26 05:35:35 +0100

    Remove wget retry

--- scripts/download_pvpoke_data.sh
+++ scripts/download_pvpoke_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O ./data/raw/pvpoke/pokemon.json --wait 10 --random-wait --continue 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/gamemaster/pokemon.json'
-wget -O ./data/raw/pvpoke/great-overall-league.json --wait 10 --random-wait --continue 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-1500.json'
-wget -O ./data/raw/pvpoke/ultra-overall-league.json --wait 10 --random-wait --continue 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-2500.json'
+wget -O ./data/raw/pvpoke/pokemon.json 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/gamemaster/pokemon.json'
+wget -O ./data/raw/pvpoke/great-overall-league.json 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-1500.json'
+wget -O ./data/raw/pvpoke/ultra-overall-league.json 'https://raw.githubusercontent.com/pvpoke/pvpoke/refs/heads/master/src/data/rankings/all/overall/rankings-2500.json'
